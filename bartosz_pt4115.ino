7746c1ac3d8ccc238822721178a33d2611598886
Karol Weyna <weyna.karol@gmail.com>
2022-11-20 19:27:48 +0100

    Update bartosz_pt4115.ino
    add independent led control

--- bartosz_pt4115.ino
+++ bartosz_pt4115.ino
@@ -6,12 +6,22 @@
 
 
 #define EEPROM_SIZE 1024
-#define LED_CHANNEL0 0
-#define GPIO_DIMMER 16
+#define LED_CHANNEL_8000K 0
+#define LED_CHANNEL_6000K 1
+#define LED_CHANNEL_FWS 2
+#define LED_CHANNEL_BLUE 3
+#define GPIO_DIMMER_8000K 16
+#define GPIO_DIMMER_6000K 17
+#define GPIO_DIMMER_FWS 4
+#define GPIO_DIMMER_BLUE 0
+#define PWM_FREQ 10000
+#define PWM_RESOLUTION 12
+#define PWM_RESOLUTION_VALUE 4095
 #define RXD0 3
 #define TXD0 1
 #define SETTINGS_ADDRESS 0
 #define START_STOP_ADDRESS 128
+#define MAX_LEVEL_ADDRESS 144
 #define I2C_SDA 21
 #define I2C_SCL 22
 struct Settings{
@@ -24,7 +34,13 @@ struct StartStop{
   byte stopHour;
   byte stopMinute;
   int dimmS;
+  byte startHourBlue;
+  byte startMinuteBlue;
+  byte stopHourBlue;
+  byte stopMinuteBlue;
+  int dimmSBlue;
 };
+byte maxLevel = 100;
 Settings settingsFlash = {
   .ssid = {'\0'},
   .password = {'\0'}
@@ -34,7 +50,12 @@ StartStop startStop = {
   .startMinute = 0,
   .stopHour = 0,
   .stopMinute = 0,
-  .dimmS = 0
+  .dimmS = 0,
+  .startHourBlue = 0,
+  .startMinuteBlue = 0,
+  .stopHourBlue = 0,
+  .stopMinuteBlue = 0,
+  .dimmSBlue = 0
 };
 const char compileDate[] = __DATE__ " " __TIME__;
 byte duty = 127;
@@ -64,13 +85,20 @@ std::vector<String> splitString(String str, char splitter){
     return result;
 }
 
+int convertPercentToPWMValue(byte percent){
+    return (int)((percent/100.0f) * PWM_RESOLUTION_VALUE);
+}
+
 void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
   if(event == ESP_SPP_SRV_OPEN_EVT){
      DateTime currentMoment = RTClib::now();
      SerialBT.printf("BT LED DRIVER 0.1 - %s - Karol Weyna\r\n", compileDate); 
      SerialBT.println("----------- CONFIG --------------------");
+     SerialBT.printf("MAX LEVEL %d\r\n", maxLevel);
+     SerialBT.printf("PWM_HZ: %d PWM_RES: %d PWM_MAX: %d\r\n", PWM_FREQ, PWM_RESOLUTION, PWM_RESOLUTION_VALUE);
      SerialBT.printf("%d.%d.%d %d:%d:%d\r\n", currentMoment.day(), currentMoment.month(), currentMoment.year(), currentMoment.hour(), currentMoment.minute(), currentMoment.second());
      SerialBT.printf("START %d:%d STOP %d:%d DIM: %d\r\n", startStop.startHour, startStop.startMinute, startStop.stopHour, startStop.stopMinute, startStop.dimmS);               
+     SerialBT.printf("BLUE START %d:%d STOP %d:%d DIM: %d\r\n", startStop.startHourBlue, startStop.startMinuteBlue, startStop.stopHourBlue, startStop.stopMinuteBlue, startStop.dimmSBlue);               
      SerialBT.printf("Name: %s\r\n", espName);
      SerialBT.printf("FLASH SIZE: %d\r\n", EEPROM.length());
      SerialBT.printf("SSID: %s PASSWORD: %s\r\n", settingsFlash.ssid, settingsFlash.password);
@@ -86,6 +114,7 @@ void setup() {
   } else {
     EEPROM.get(SETTINGS_ADDRESS, settingsFlash);
     EEPROM.get(START_STOP_ADDRESS, startStop);
+    EEPROM.get(MAX_LEVEL_ADDRESS, maxLevel);
     //WiFi.mode(WIFI_STA);
     //WiFi.begin(settingsFlash.ssid, settingsFlash.password);
     //SerialBT.print("Connecting to WiFi ..");
@@ -101,9 +130,24 @@ void setup() {
   SerialBT.begin(espName);
   SerialBT.setPin(pinChar);
   SerialBT.register_callback(callback);
-  ledcSetup(LED_CHANNEL0, freq, resolution);
-  ledcAttachPin(GPIO_DIMMER, LED_CHANNEL0);
-  ledcWrite(LED_CHANNEL0, duty);
+  //--------------- 8000K --------------------------- //
+  ledcSetup(LED_CHANNEL_8000K, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(GPIO_DIMMER_8000K, LED_CHANNEL_8000K);
+  //--------------- 6000K --------------------------- //
+  ledcSetup(LED_CHANNEL_6000K, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(GPIO_DIMMER_6000K, LED_CHANNEL_6000K);
+  //--------------- FWS --------------------------- //
+  ledcSetup(LED_CHANNEL_FWS, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(GPIO_DIMMER_FWS, LED_CHANNEL_FWS);
+  //--------------- BLUE --------------------------- //
+  ledcSetup(LED_CHANNEL_BLUE, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(GPIO_DIMMER_BLUE, LED_CHANNEL_BLUE);
+  //------------------------------------------------//
+  byte startDuty = convertPercentToPWMValue(10);
+  ledcWrite(LED_CHANNEL_8000K, startDuty);
+  ledcWrite(LED_CHANNEL_6000K, startDuty);
+  ledcWrite(LED_CHANNEL_FWS, startDuty);
+  ledcWrite(LED_CHANNEL_BLUE, startDuty);
   Wire.begin(I2C_SDA, I2C_SCL);
 }
 
@@ -133,23 +177,36 @@ byte setProperLight(long currentTimestamp, long startTimestamp, long stopTimesta
     }
     return 0;
 }
-
+long millis250msInterval = 0;
 void loop() {
-    DateTime currentMoment = RTClib::now();
-    DateTime startMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.startHour, startStop.startMinute, 0);
-    DateTime stopMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day() + 1, startStop.stopHour, startStop.stopMinute, 0);
-    if(startStop.startHour <= startStop.stopHour){
-      stopMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.stopHour, startStop.stopMinute, 0);
+
+    if(millis() > millis250msInterval + 250){
+      millis250msInterval = millis();
+      DateTime currentMoment = RTClib::now();
+      DateTime startMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.startHour, startStop.startMinute, 0);
+      DateTime stopMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day() + 1, startStop.stopHour, startStop.stopMinute, 0);
+      DateTime startMomentBlue = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.startHourBlue, startStop.startMinuteBlue, 0);
+      DateTime stopMomentBlue = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day() + 1, startStop.stopHourBlue, startStop.stopMinuteBlue, 0);
+      if(startStop.startHour <= startStop.stopHour){
+        stopMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.stopHour, startStop.stopMinute, 0);
+        stopMomentBlue = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.stopHourBlue, startStop.stopMinuteBlue, 0);
+      }
+      byte percent6000K = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), maxLevel, startStop.dimmS);
+      byte percent8000K = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), maxLevel, startStop.dimmS);
+      byte percentFWS = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), maxLevel, startStop.dimmS);
+      byte percentBlue = setProperLight(currentMoment.unixtime(), startMomentBlue.unixtime(), stopMomentBlue.unixtime(), maxLevel, startStop.dimmSBlue);
+      int duty6000K = convertPercentToPWMValue(percent6000K);
+      int duty8000K = convertPercentToPWMValue(percent8000K);
+      int dutyFWS = convertPercentToPWMValue(percentFWS);
+      int dutyBlue = convertPercentToPWMValue(percentBlue);
+      ledcWrite(LED_CHANNEL_6000K, duty6000K);
+      ledcWrite(LED_CHANNEL_8000K, duty8000K);
+      ledcWrite(LED_CHANNEL_FWS, dutyFWS);
+      ledcWrite(LED_CHANNEL_BLUE, dutyBlue);
+
+
+      SerialBT.printf("6000K %d 8000K %d FWS %d BLUE %d\r\n", duty6000K, duty8000K, dutyFWS, dutyBlue);
     }
-    byte percent6000K = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), 100, startStop.dimmS);
-    byte percent8000K = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), 100, startStop.dimmS);
-    byte percentFWS = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), 100, startStop.dimmS);
-    byte percentBlue = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), 100, startStop.dimmS);
-    byte duty6000K = (byte)((percent6000K/100.0f) * 255);
-    byte duty8000K = (byte)((percent8000K/100.0f) * 255);
-    byte dutyFWS = (byte)((percentFWS/100.0f) * 255);
-    byte dutyBlue = (byte)((percentBlue/100.0f) * 255);
-    ledcWrite(LED_CHANNEL0, duty6000K);
     if(SerialBT.available())
     {
         try {
@@ -173,11 +230,10 @@ void loop() {
                 ESP.restart();
             }
             if(command.equals("setbright")){
-              int x = splittedString[2].toInt();
-              byte p = (byte)((x/100.0f) * 255);
-              duty = p;
-              SerialBT.printf("SETBRIGHT - %d RAW(%d)\r\n", x, p);
-              ledcWrite(LED_CHANNEL0, duty);
+              int channel = splittedString[2].toInt();
+              int percent = splittedString[3].toInt();
+              SerialBT.printf("SETBRIGHT - CHANNEL %d PERCENT %d\r\n", channel, percent);
+              ledcWrite(channel, convertPercentToPWMValue(percent));
             }
             if(command.equals("setdate")){
               int day = splittedString[2].toInt();
@@ -203,14 +259,32 @@ void loop() {
               int stopHour = splittedString[4].toInt();
               int stopMinute = splittedString[5].toInt();
               int dimmS = splittedString[6].toInt();
+              int startHourBlue = splittedString[7].toInt();
+              int startMinuteBlue = splittedString[8].toInt();
+              int stopHourBlue = splittedString[9].toInt();
+              int stopMinuteBlue = splittedString[10].toInt();
+              int dimmSBlue = splittedString[11].toInt();
               startStop.startHour = startHour;
               startStop.startMinute = startMinute;
               startStop.stopHour = stopHour;
               startStop.stopMinute = stopMinute;
               startStop.dimmS = dimmS;
+              startStop.startHourBlue = startHourBlue;
+              startStop.startMinuteBlue = startMinuteBlue;
+              startStop.stopHourBlue = stopHourBlue;
+              startStop.stopMinuteBlue = stopMinuteBlue;
+              startStop.dimmSBlue = dimmSBlue;
               EEPROM.put(START_STOP_ADDRESS, startStop);
               EEPROM.commit();
-              SerialBT.printf("START %d:%d STOP %d:%d\r\n", startHour, startMinute, stopHour, stopMinute);               
+              SerialBT.printf("START %d:%d STOP %d:%d\r\n", startHour, startMinute, stopHour, stopMinute);
+              SerialBT.printf("START BLUE %d:%d STOP %d:%d\r\n", startHourBlue, startMinuteBlue, stopHourBlue, stopMinuteBlue);                          
+            }
+            if(command.equals("maxlevel")){
+              int maxL = splittedString[2].toInt();
+              maxLevel = maxL;
+              EEPROM.put(MAX_LEVEL_ADDRESS, maxLevel);
+              EEPROM.commit();
+              SerialBT.printf("MAX LEVEL SET %d\r\n", maxL);                          
             }
          } else {
            SerialBT.println("WRONG PASSWORD");
