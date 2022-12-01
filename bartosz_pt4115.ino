b9cb6b002824fa762fbeabf257ea4570bc2b1363
Karol Weyna <weyna.karol@gmail.com>
2022-12-01 10:12:58 +0100

    Add sync time with ntp server, add soft start, add support relay

--- bartosz_pt4115.ino
+++ bartosz_pt4115.ino
@@ -15,6 +15,7 @@
 #define GPIO_DIMMER_6000K 17
 #define GPIO_DIMMER_FWS 4
 #define GPIO_DIMMER_BLUE 0
+#define GPIO_RELAY 15
 #define PWM_FREQ 10000
 #define PWM_RESOLUTION 12
 #define PWM_RESOLUTION_VALUE 4095
@@ -23,6 +24,7 @@
 #define SETTINGS_ADDRESS 0
 #define START_STOP_ADDRESS 128
 #define MAX_LEVEL_ADDRESS 144
+#define NTP_SETTINGS_ADDRESS 146
 #define I2C_SDA 21
 #define I2C_SCL 22
 struct Settings{
@@ -45,7 +47,11 @@ struct MaxLevels{
   byte white;
   byte blue;
 };
-
+struct TimeSettings{
+  int gmtOffset_sec;
+  int daylightOffset_sec;
+  char ntpServer[64];
+};
 
 Settings settingsFlash = {
   .ssid = {'\0'},
@@ -67,10 +73,13 @@ MaxLevels maxLevels = {
   .white = 0,
   .blue = 0
 };
-const char* ntpServer = "pool.ntp.org";
+TimeSettings timeSettings = {
+  .gmtOffset_sec = 3600,
+  .daylightOffset_sec = 3600,
+  .ntpServer = {'p','o','o','l','.','n','t','p','.','o','r','g','\0'}
+};
+
 const char compileDate[] = __DATE__ " " __TIME__;
-const long gmtOffset_sec = 3600;
-const int daylightOffset_sec = 3600;
 byte duty = 127;
 int freq = 10000;                                     
 byte resolution = 8;
@@ -79,16 +88,6 @@ BluetoothSerial SerialBT;
 char espName[32];
 char pinChar[9];
 DS3231 myRTC;
-
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    SerialBT.println("Failed to obtain time");
-    return;
-  }
-  SerialBT.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
 
 std::vector<String> splitString(String str, char splitter){
     std::vector<String> result;
@@ -117,6 +116,7 @@ void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
      DateTime currentMoment = RTClib::now();
      SerialBT.printf("BT LED DRIVER 0.1 - %s - Karol Weyna\r\n", compileDate); 
      SerialBT.println("----------- CONFIG --------------------");
+     SerialBT.printf("NTP SETTINGS GMT: %d DAYLIGHT: %d, SERVER: %s\r\n", timeSettings.gmtOffset_sec, timeSettings.daylightOffset_sec, timeSettings.ntpServer);
      SerialBT.printf("MAX LEVEL WHITE %d BLUE %d\r\n", maxLevels.white, maxLevels.blue);
      SerialBT.printf("PWM_HZ: %d PWM_RES: %d PWM_MAX: %d\r\n", PWM_FREQ, PWM_RESOLUTION, PWM_RESOLUTION_VALUE);
      SerialBT.printf("%d.%d.%d %d:%d:%d\r\n", currentMoment.day(), currentMoment.month(), currentMoment.year(), currentMoment.hour(), currentMoment.minute(), currentMoment.second());
@@ -132,12 +132,14 @@ void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
 }
 
 void setup() {
+  Serial.begin(115200);
   if(!EEPROM.begin(EEPROM_SIZE)){
     SerialBT.println("failed to initialise EEPROM");
   } else {
     EEPROM.get(SETTINGS_ADDRESS, settingsFlash);
     EEPROM.get(START_STOP_ADDRESS, startStop);
     EEPROM.get(MAX_LEVEL_ADDRESS, maxLevels);
+    EEPROM.get(NTP_SETTINGS_ADDRESS, timeSettings);
   }
   
   uint64_t pin = ((((uint64_t)ESP.getEfuseMac() ^ 0xFFFFFFFFFFFFFFFF) + 2137) % 100000000);
@@ -164,19 +166,70 @@ void setup() {
   ledcWrite(LED_CHANNEL_6000K, startDuty);
   ledcWrite(LED_CHANNEL_FWS, startDuty);
   ledcWrite(LED_CHANNEL_BLUE, startDuty);
+  pinMode(GPIO_RELAY, OUTPUT);
+  digitalWrite(GPIO_RELAY, HIGH);
   Wire.begin(I2C_SDA, I2C_SCL);
 
   // ------------------ SYNC TIME ------------------------------- //
   WiFi.mode(WIFI_STA);
   WiFi.begin(settingsFlash.ssid, settingsFlash.password);
-  SerialBT.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
+  Serial.println("Connecting to WiFi ..");
+  while(millis() < 5000){
+    Serial.println("...");
+    delay(500);
   }
-  Serial.println(WiFi.localIP());
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  WiFi.disconnect(true);
+  delay(1000);
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("CONNECTED");
+    Serial.println(WiFi.localIP());
+    configTime(timeSettings.gmtOffset_sec, timeSettings.daylightOffset_sec, timeSettings.ntpServer);
+    struct tm timeinfo;
+    if(getLocalTime(&timeinfo)){
+      Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+      bool updateTime = false;
+      ///////
+      DateTime currentMoment = RTClib::now();
+      if(abs(currentMoment.second() - timeinfo.tm_sec) > 10){
+        Serial.printf("Roznica sekund %d vs %d \r\n", currentMoment.second(), timeinfo.tm_sec);  
+        updateTime = true;
+      }
+      if(currentMoment.minute() != timeinfo.tm_min){
+        Serial.printf("Roznica minut %d vs %d \r\n", currentMoment.minute(), timeinfo.tm_min);  
+        updateTime = true;
+      }
+      if(currentMoment.hour() != timeinfo.tm_hour){
+       Serial.printf("Roznica godzin %d vs %d \r\n", currentMoment.hour(), timeinfo.tm_hour);  
+        updateTime = true;
+      }
+      if(currentMoment.day() != timeinfo.tm_mday){
+        Serial.printf("Roznica dni %d vs %d \r\n", currentMoment.day(), timeinfo.tm_mday);  
+        updateTime = true;
+      }
+      if(currentMoment.month() != (timeinfo.tm_mon + 1)){
+        Serial.printf("Roznica miesiecy %d vs %d \r\n", currentMoment.month(), (timeinfo.tm_mon + 1));  
+        updateTime = true;
+      }
+      if(currentMoment.year() != (timeinfo.tm_year + 1900)){
+        Serial.printf("Roznica lat %d vs %d \r\n", currentMoment.year(), (timeinfo.tm_year + 1900));  
+        updateTime = true;
+      }
+      
+
+      if(updateTime == true){
+        myRTC.setClockMode(false);//24-hour mode
+        myRTC.setSecond(timeinfo.tm_sec);
+        myRTC.setMinute(timeinfo.tm_min);
+        myRTC.setHour(timeinfo.tm_hour);
+        myRTC.setDate(timeinfo.tm_mday);
+        myRTC.setMonth((timeinfo.tm_mon + 1));
+        myRTC.setYear((timeinfo.tm_year - 100));
+        Serial.printf("UPDATE TIME - SET TIME TO %d.%d.%d %d:%d:%d\r\n", (timeinfo.tm_year + 1900), (timeinfo.tm_mon + 1), timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+      }
+    } else {
+      Serial.println("Failed to obtain time");
+    }
+    WiFi.disconnect(true);
+  }
   WiFi.mode(WIFI_OFF);
   // ----------------------------------------------------------- //
 }
@@ -219,12 +272,29 @@ void loop() {
       DateTime stopMomentBlue = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day() + 1, startStop.stopHourBlue, startStop.stopMinuteBlue, 0);
       if(startStop.startHour <= startStop.stopHour){
         stopMoment = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.stopHour, startStop.stopMinute, 0);
+      }
+      if(startStop.startHourBlue <= startStop.stopHourBlue){
         stopMomentBlue = DateTime(currentMoment.year(), currentMoment.month(), currentMoment.day(), startStop.stopHourBlue, startStop.stopMinuteBlue, 0);
       }
       byte percent6000K = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), maxLevels.white, startStop.dimmS);
       byte percent8000K = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), maxLevels.white, startStop.dimmS);
       byte percentFWS = setProperLight(currentMoment.unixtime(), startMoment.unixtime(), stopMoment.unixtime(), maxLevels.white, startStop.dimmS);
       byte percentBlue = setProperLight(currentMoment.unixtime(), startMomentBlue.unixtime(), stopMomentBlue.unixtime(), maxLevels.blue, startStop.dimmSBlue);
+      long secondsFromStart = millis()/1000;
+      if(secondsFromStart <= percent6000K){
+        percent6000K = secondsFromStart;
+      }
+      if(secondsFromStart <= percent8000K){
+        percent8000K = secondsFromStart;
+      }
+      if(secondsFromStart <= percentFWS){
+        percentFWS = secondsFromStart;
+      }
+      if(secondsFromStart <= percentBlue){
+        percentBlue = secondsFromStart;
+      }
+      
+      
       int duty6000K = convertPercentToPWMValue(percent6000K);
       int duty8000K = convertPercentToPWMValue(percent8000K);
       int dutyFWS = convertPercentToPWMValue(percentFWS);
@@ -233,8 +303,6 @@ void loop() {
       ledcWrite(LED_CHANNEL_8000K, duty8000K);
       ledcWrite(LED_CHANNEL_FWS, dutyFWS);
       ledcWrite(LED_CHANNEL_BLUE, dutyBlue);
-
-
       SerialBT.printf("6000K %d 8000K %d FWS %d BLUE %d\r\n", duty6000K, duty8000K, dutyFWS, dutyBlue);
     }
     if(SerialBT.available())
@@ -317,6 +385,16 @@ void loop() {
               EEPROM.put(MAX_LEVEL_ADDRESS, maxLevels);
               EEPROM.commit();
               SerialBT.printf("MAX LEVEL SET WHITE %d BLUE \r\n", maxWhite, maxBlue);                          
+            }
+            if(command.equals("setntp")){
+                splittedString[2].toCharArray(timeSettings.ntpServer, 64);
+                int gmt = splittedString[3].toInt();
+                int daylight = splittedString[4].toInt();
+                timeSettings.gmtOffset_sec = gmt;
+                timeSettings.daylightOffset_sec = daylight;
+                EEPROM.put(NTP_SETTINGS_ADDRESS, timeSettings);
+                EEPROM.commit();
+                SerialBT.printf("SET NTP GMT: %d DAYLIGHT: %d SERVER: %s\r\n", timeSettings.gmtOffset_sec, timeSettings.daylightOffset_sec, timeSettings.ntpServer);
             }
          } else {
            SerialBT.println("WRONG PASSWORD");
